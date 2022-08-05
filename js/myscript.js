507300d89ed5f5c17fcaf8c052334f833181b60c
aftershock06 <tdbranchii@gmail.com>
2022-08-05 19:53:55 -0400

    updated sscripts

--- js/myscript.js
+++ js/myscript.js
@@ -22,5 +22,14 @@ $(document).ready(function(){
         document.getElementById('temp').innerHTML=temp;
         document.getElementById('soil').innerHTML=soil;
         document.getElementById('update').innerHTML=date;
-   }         
+   }  
+     
 })
+
+
+function differenceInWeeks(d1, d2) {
+    var t2 = d2.getTime();
+    var t1 = d1.getTime();
+
+    return parseInt((t2-t1)/(24*3600*1000*7));
+  }
