b7a0844281e3e6a3579ef588f418f515e2e34d54
aftershock06 <tdbranchii@gmail.com>
2023-04-07 21:17:05 -0400

    adding python file

---
+++ helloWorldTemplate.py
@@ -0,0 +1,28 @@
+'''
+Code Created by Taurean Branch
+'''
+
+from flask import Flask, render_template
+import datetime
+
+app = Flask(__name__)
+@app.route('/')
+
+def index():
+        now = datetime.datetime.now()
+        timeString = now.strftime("%m-%d-%Y %H:%M")
+        templateData =  {
+                'title': 'Hello',
+                'time' : timeString
+                }
+        return render_template('index.html', **templateData)
+
+
+if __name__ == '__main__':
+        app.run(debug=True, port=80, host='0.0.0.0')
+
+
+
+
+
+
