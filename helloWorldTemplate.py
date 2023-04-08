6d8b62344d475e4c758f523ebaf8a07ae9276e9e
aftershock06 <tdbranchii@gmail.com>
2023-04-07 21:31:00 -0400

    trying to update python file

--- helloWorldTemplate.py
+++ helloWorldTemplate.py
@@ -4,6 +4,7 @@ Code Created by Taurean Branch
 
 from flask import Flask, render_template, request, jsonify
 import datetime
+import csv
 
 app = Flask(__name__)
 @app.route('/')
@@ -17,6 +18,25 @@ def index():
                 }
         return render_template('index.html', **templateData)
 
+
+@app.route('/gardendata', methods=['POST'])
+def handle_gardendata():
+    data = request.json
+    temperature = data['temperature']
+    humidity = data['humidity']
+    light = data['light']
+
+    with open('gardendata.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([temperature, humidity, light])
+
+    response_data = {
+        'temperature': temperature
+        'humidity' : humidity
+        'light' : light
+    }
+
+    return jsonify(response_data)
 
 if __name__ == '__main__':
         app.run(debug=True, port=80, host='0.0.0.0')
